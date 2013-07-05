818fc5d24e73ae7d276539d68ad37ba177f8f714
yeojoy <yeojoy.kim@gmail.com>
2013-07-05 15:36:57 +0900

    Drop API key 변경. 예제 분석 필요

--- src/me/yeojoy/myotaapp/MainActivity.java
+++ src/me/yeojoy/myotaapp/MainActivity.java
@@ -1,20 +1,30 @@
 
 package me.yeojoy.myotaapp;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
-
-import me.yeojoy.myotaapp.fragment.MainFragment;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
-public class MainActivity extends SherlockFragmentActivity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session.AccessType;
 
+import me.yeojoy.myotaapp.fragment.MainFragment;
+
+public class MainActivity extends SherlockFragmentActivity implements Constants {
+
+    private final static AccessType ACCESS_TYPE = AccessType.DROPBOX;
     
     private FragmentManager mFragmentManager;
+    
+    
+    public DropboxAPI<AndroidAuthSession> mDropboxApi;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +34,11 @@ public class MainActivity extends SherlockFragmentActivity {
         mFragmentManager = getSupportFragmentManager();
         Fragment mainFragment = new MainFragment();
         
-        mFragmentManager.beginTransaction().add(R.id.rl_frag_container, mainFragment, "Main_Fragment").commit();
+        mFragmentManager.beginTransaction().add(R.id.rl_frag_container, mainFragment, FRAGMENT_TAG_MAIN).commit();
         
+        AppKeyPair appKeys = new AppKeyPair(DROPBOX_APP_KEY, DROPBOX_APP_SECRET);
+        AndroidAuthSession dropboxSession = new AndroidAuthSession(appKeys, ACCESS_TYPE);
+        mDropboxApi = new DropboxAPI<AndroidAuthSession>(dropboxSession);
     }
     
     @Override
@@ -38,8 +51,26 @@ public class MainActivity extends SherlockFragmentActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            
+            case R.id.action_conn_dropbox :
+                
+                mDropboxApi.getSession().startAuthentication(MainActivity.this);
+                
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mDropboxApi.getSession().authenticationSuccessful()) {
+            try {
+                mDropboxApi.getSession().finishAuthentication();
+                AccessTokenPair tokens = mDropboxApi.getSession().getAccessTokenPair();
+            } catch (IllegalStateException e) {
+                Log.i("DbAuthLog", "Error authenticating", e);
+            }
+        }
+        
     }
 }
--- src/me/yeojoy/myotaapp/fragment/MainFragment.java
+++ src/me/yeojoy/myotaapp/fragment/MainFragment.java
@@ -1,25 +1,29 @@
 package me.yeojoy.myotaapp.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
 import com.actionbarsherlock.app.SherlockFragment;
 
 import me.yeojoy.myotaapp.R;
 import me.yeojoy.myotaapp.adapter.MainAdapter;
 import me.yeojoy.myotaapp.model.AppEntity;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainFragment extends SherlockFragment {
+public class MainFragment extends SherlockFragment implements OnClickListener {
     
     private ListView mLvAppList;
     private MainAdapter mAppAdapter;
     private List<AppEntity> mAppList;
+    
+    private Button mBtnGet;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,7 +49,26 @@ public class MainFragment extends SherlockFragment {
         mAppAdapter = new MainAdapter(getActivity(), mAppList);
         mLvAppList.setAdapter(mAppAdapter);
         
+        mBtnGet = (Button) view.findViewById(R.id.btn_get_app_list);
+        mBtnGet.setOnClickListener(this);
+        
         return view;
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+        
+        switch (v.getId()) {
+            case R.id.btn_get_app_list :
+                
+                break;
+        }
     }
     
 }
